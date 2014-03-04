caa7abeacda1e48faceee401a8032fefaea7babf
Francesco Siddi <francesco.siddi@gmail.com>
2014-03-04 16:09:39 +0100

    Initial commit

---
+++ footer.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+?>
+
+		</div><!-- #main -->
+		<footer id="colophon" class="site-footer" role="contentinfo">
+			<?php get_sidebar( 'main' ); ?>
+
+			<div class="site-info">
+				<span>2014 Blender Institute</span> - <a href="/sharing/">Copyright and sharing info</a> - <a href="https://plus.google.com/101069217366098079903" rel="publisher">Google+</a>
+			</div><!-- .site-info -->
+		</footer><!-- #colophon -->
+	</div><!-- #page -->
+
+	<?php wp_footer(); ?>
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-1418081-9', 'blender.org');
+	  ga('send', 'pageview');
+
+	</script>
+</body>
+</html>
